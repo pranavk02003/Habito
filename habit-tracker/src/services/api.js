fb74c7c95fa65d0ec334202073760ed2cacbecde
Pranav <pranavk02003@gmail.com>
2026-03-17 20:51:57 +0530

    Updated API URL for deployment

--- habit-tracker/src/services/api.js
+++ habit-tracker/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://habito-1c5c.onrender.com"
 });
 
 API.interceptors.request.use((req) => {
